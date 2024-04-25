3340261469efcdd37cc0bb552227c5f6cb137d26
BrianOat <114760844+BrianOat@users.noreply.github.com>
2024-04-25 09:49:50 -0400

    Update kernel.c

--- src/C/kernel.c
+++ src/C/kernel.c
@@ -1,3 +1,17 @@
-int main(){
-    return 0;
+/* kernel.c */
+#include <stdio.h>
+
+/* Intialize Hardware*/
+void init_hardware() {
+    printf("Hello");
+}
+
+/* Kernel entry point */
+void kernel_main() {
+
+    init_hardware();
+
+    /* Kernel Logic here */
+    
+    printf("Hardware initialized!");
 }
